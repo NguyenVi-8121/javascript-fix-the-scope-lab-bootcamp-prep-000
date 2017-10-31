eba7fb3595dfb52ccb694a537d6ce51935bfe9f5
Vi Nguyen <nguyenvi.8121@gmail.com>
2017-10-31 05:51:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal(animal) {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
-  console.log ("x")
+  console.log ("myAnimal")
 }
 
 function add2(n) {
